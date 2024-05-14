684a3782016c1da70b3ccbc406969b23378e8ed7
SomEnaMeforme <98802409+SomEnaMeforme@users.noreply.github.com>
2024-05-14 22:49:54 +0500

    Load props from file

--- src/main/java/core/utils/Utils.java
+++ src/main/java/core/utils/Utils.java
@@ -2,18 +2,29 @@ package core.utils;
 
 import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.io.IOException;
 import java.util.Properties;
 
 public class Utils {
     public static class PropertiesLoader {
 
         public static Properties GetProperties() {
-            //TODO: в будущем должен получать путь к конфигу и сериализовать его в настройки
-            String kafkaServer = "127.0.0.1:29092";
             Properties properties = new Properties();
-            properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
-            properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-            properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+            var inputStream = PropertiesLoader.class.getClassLoader().getResourceAsStream("connection.properties");
+            try {
+                properties.load(inputStream);
+            }
+            catch (IOException exp) {
+                System.out.println("Exception: " + exp);
+            }
+            finally {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
             return properties;
         }
 
